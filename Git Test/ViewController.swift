0d3c35b5e28fe77484f8eb8120f11e3949851278
Selcuk Albut <salbut@gmail.com>
2019-02-04 18:17:19 +0300

    My first commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by AlBuT on 4.02.2019.
+//  Copyright © 2019 AlBuT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("github")
+    }
+
+
+}
+
